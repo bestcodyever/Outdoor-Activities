b2a51f16f68d5f390aa1868a7e15176dd740c4bc
Cody Duskin <codyduskin@me.com>
2017-10-06 11:23:52 -0600

    better functions

--- app.js
+++ app.js
@@ -35,19 +35,6 @@ $('#hikeButton').click(function(event) {
   $('.hike-dis').fadeIn(2000)
 
 })
-function reset() {
-  $('.movieLoad').fadeIn(200)
-  $('.hikeLoad').fadeIn(200)
-  $('.hike-city').html('')
-  $('.hike-dir').html('')
-  $('.hike-dis').attr('src', '')
-  $('#title').css({"display": "none"})
-  $('#plot').css({"display": "none"})
-  $('#title').html('')
-  $('#plot').html('')
-  $('#hikeButton').css({"display": "none"})
-  $('#movieButton').css({"display": "none"})
-}
 
 function random(z) {
   return Math.floor(Math.random() * z)
@@ -65,7 +52,7 @@ $("#click").click(function(event) {
   }).then(function(url) {
     let lat = url.current_observation.display_location.latitude
     let lon = url.current_observation.display_location.longitude
-    let trailUrl = 'https://trailapi-trailapi.p.mashape.com/?lat=' + lat + '&lon=' + lon +
+    var trailUrl = 'https://trailapi-trailapi.p.mashape.com/?lat=' + lat + '&lon=' + lon +
      '&q[activities_activity_type_name_eq]=hiking&radius=45'
     $.ajax({
       url: trailUrl,
@@ -85,17 +72,10 @@ $("#click").click(function(event) {
       }
     })
     if ($('.temp_f')[0].innerText <= lowTemp || $('.temp_f')[0].innerText > highTemp) {
-      $('.movieLoad').fadeOut()
-      $('#title').fadeIn(2000)
-      $('#plot').fadeIn(2000)
-      $('.hikeLoad').css({"display": "none"})
-      $('#hikeButton').fadeIn()
-    } else {
-      $('.hike-city').fadeIn(2000)
-      $('.hike-dir').fadeIn(2000)
-      $('.hike-dis').fadeIn(2000)
-      $('.movieLoad').css({"display": "none"})
-      $('#movieButton').fadeIn()
+      displayBadWeather()
+    }
+    else {
+      displayGoodWeather()
     }
   })
   $.get(movieURL + movies[random(19)], function(movie) {
@@ -126,3 +106,31 @@ const movies = [
   'mean+girls',
   'gump'
 ]
+
+function displayGoodWeather(){
+  $('.hike-city').fadeIn(2000)
+  $('.hike-dir').fadeIn(2000)
+  $('.hike-dis').fadeIn(2000)
+  $('.movieLoad').css({"display": "none"})
+  $('#movieButton').fadeIn()
+}
+function displayBadWeather(){
+  $('.movieLoad').fadeOut()
+  $('#title').fadeIn(2000)
+  $('#plot').fadeIn(2000)
+  $('.hikeLoad').css({"display": "none"})
+  $('#hikeButton').fadeIn()
+}
+function reset() {
+  $('.movieLoad').fadeIn(200)
+  $('.hikeLoad').fadeIn(200)
+  $('.hike-city').html('')
+  $('.hike-dir').html('')
+  $('.hike-dis').attr('src', '')
+  $('#title').css({"display": "none"})
+  $('#plot').css({"display": "none"})
+  $('#title').html('')
+  $('#plot').html('')
+  $('#hikeButton').css({"display": "none"})
+  $('#movieButton').css({"display": "none"})
+}
